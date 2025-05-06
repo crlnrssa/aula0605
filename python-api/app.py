eb652b807609f171a84f3589d96dd2fa2f813a9a
crlnrssa <143560004+crlnrssa@users.noreply.github.com>
2025-05-06 20:56:21 -0300

    first commit

---
+++ python-api/app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+app = Flask(__name__)
+
+# print(os.getenv("DB_USER"))
+
+@app.route("/")
+def hello():
+    db = mysql.connector.connect(
+        user=os.getenv("DB_USER"),
+        host=os.getenv("DB_HOST"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT 'Hello from Flask and MySQL!'")
+    result = cursor.fetchone()
+    cursor.close()
+    db.close()
+    return result[0]
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+    
